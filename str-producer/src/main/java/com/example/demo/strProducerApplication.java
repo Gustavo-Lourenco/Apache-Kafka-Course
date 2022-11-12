42d59db77f7ed68a4d0b6ccedfe5e48a9db8404c
Gustavo Lourenço <gustavo.lourenco@dbccompany.com.br>
2022-11-12 15:46:29 -0300

    str-producer started

---
+++ str-producer/src/main/java/com/example/demo/strProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class strProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(strProducerApplication.class, args);
+	}
+
+}
