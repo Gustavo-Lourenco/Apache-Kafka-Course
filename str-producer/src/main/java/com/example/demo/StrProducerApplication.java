ebf426afc74e759f1d7e6f38be5e1fefe301ea1f
Gustavo Lourenço <gustavo.lourenco@dbccompany.com.br>
2022-11-14 15:01:46 -0300

    Adiciona callback para a funcao sendMessage

--- str-producer/src/main/java/com/example/demo/StrProducerApplication.java
+++ str-producer/src/main/java/com/example/demo/StrProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class strProducerApplication {
+public class StrProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(strProducerApplication.class, args);
+		SpringApplication.run(StrProducerApplication.class, args);
 	}
 
 }
---
+++ str-producer/src/main/java/com/example/demo/services/StringProducerService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic", message).addCallback(
+                succes -> {
+                    if(succes != null){
+                        log.info("Send Message with sucess {}", message);
+                        log.info("Partition {}, offset{}",
+                            succes.getRecordMetadata().partition(),
+                            succes.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
+    }
+
+}
